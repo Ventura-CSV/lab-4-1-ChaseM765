bca29f04459063a7ffabf6c8820d30bcebab588a
ChaseM765 <ChaseM765>
2024-06-27 23:57:49 -0700

    for statement added

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ def main():
     N = int(input('Enter the number N: '))
     result = []
 
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    for x in range(0,N+1):
+        math = 2**x
+        result.append(math)
+    print(*result)
 
     ########################################
     # Do not delete the return statement
